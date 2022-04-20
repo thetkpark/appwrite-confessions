a4adb5bef3c26a6cc40df187a0a14db6957cdc8b
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-04-20 22:42:56 +0700

    change time format

--- components/Confession.js
+++ components/Confession.js
@@ -6,14 +6,11 @@ const Confession = ({ content, date }) => {
 		<Card sx={{ minWidth: 275, maxWidth: 512 }}>
 			<CardContent>
 				<Typography variant="caption" sx={{ mb: 1.5 }} color="text.secondary">
-					{dayjs(date).format("MMMM DD, YYYY")}
+					{dayjs(date).format("MMMM D, YYYY h:mm A")}
 				</Typography>
 				<br />
 				<Typography variant="body">{content}</Typography>
 			</CardContent>
-			{/* <CardActions>
-				<Button size="small">Learn More</Button>
-			</CardActions> */}
 		</Card>
 	)
 }
